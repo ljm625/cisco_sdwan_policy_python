b12441d7198f4944777a6d4f3ff62436d34d7957
Jiaming Li <jiaminli@cisco.com>
2019-11-20 12:59:45 +0800

    Update version to v0.3

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from distutils.core import setup
 setup(
   name = 'cisco-sdwan-policy',         # How you named your package folder (MyLib)
   packages = ['cisco_sdwan_policy'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A module for easy add/modify policies to Cisco SD-WAN(Viptela)',   # Give a short description about your library
   author = 'Jiaming Li',                   # Type in your name
   author_email = 'jiaminli@cisco.com',      # Type in your E-Mail
   url = 'https://github.com/ljm625/cisco_sdwan_policy_python',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ljm625/cisco_sdwan_policy_python/archive/v0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ljm625/cisco_sdwan_policy_python/archive/v0.3.tar.gz',    # I explain this later on
   keywords = ['SDWAN', 'CiSCO', 'REST'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
       ],
+  scripts=['bin/sdwan-policy-tool','bin/sdwan-template-tool'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Libraries',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
